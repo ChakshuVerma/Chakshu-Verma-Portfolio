4307c827abe440ea31dab7cd0cc7e17b9c31d796
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-06-16 16:58:14 +0530

    Form Update

--- script.js
+++ script.js
@@ -74,38 +74,38 @@ $(document).ready(function(){
     //     e.preventDefault(); //preventing from submitting form
     // });
 
-    $('.send-msg').click(()=>{
-        $fullname = $('.fullname').val();
-        $email = $('.email-input').val();
-        $subject = $('.subject').val();
-        $message = $('.message').val();
-        $('.send-msg').text("Sending...");
-        $('.contact-form').addClass("disable");
+    // $('.send-msg').click(()=>{
+    //     $fullname = $('.fullname').val();
+    //     $email = $('.email-input').val();
+    //     $subject = $('.subject').val();
+    //     $message = $('.message').val();
+    //     $('.send-msg').text("Sending...");
+    //     $('.contact-form').addClass("disable");
 
-        $.ajax({
-            url: "mail.php",
-            type: "POST",
-            data: "email="+$email+"&subject="+$subject+"&message="+$message,
-            success: function(data){
-                $errorBox = $('.error-box');
-                $('.send-msg').text("Send message");
-                $('.contact-form').removeClass("disable");
-                if(data == "success"){
-                    $fullname = $('.fullname').val("");
-                    $email = $('.email-input').val("");
-                    $subject = $('.subject').val("");
-                    $message = $('.message').val("");
-                    $errorBox.html("Your message has been sent!");
-                    $errorBox.addClass("success");
-                    setTimeout(()=>{
-                        $errorBox.html("");
-                        $errorBox.removeClass("success");
-                    }, 5000);
-                }else{
-                    $errorBox.removeClass("success");
-                    $errorBox.html("<span>* </span>" + data);
-                }
-            }
-        });
-    });
+    //     $.ajax({
+    //         url: "mail.php",
+    //         type: "POST",
+    //         data: "email="+$email+"&subject="+$subject+"&message="+$message,
+    //         success: function(data){
+    //             $errorBox = $('.error-box');
+    //             $('.send-msg').text("Send message");
+    //             $('.contact-form').removeClass("disable");
+    //             if(data == "success"){
+    //                 $fullname = $('.fullname').val("");
+    //                 $email = $('.email-input').val("");
+    //                 $subject = $('.subject').val("");
+    //                 $message = $('.message').val("");
+    //                 $errorBox.html("Your message has been sent!");
+    //                 $errorBox.addClass("success");
+    //                 setTimeout(()=>{
+    //                     $errorBox.html("");
+    //                     $errorBox.removeClass("success");
+    //                 }, 5000);
+    //             }else{
+    //                 $errorBox.removeClass("success");
+    //                 $errorBox.html("<span>* </span>" + data);
+    //             }
+    //         }
+    //     });
+    // });
 });
